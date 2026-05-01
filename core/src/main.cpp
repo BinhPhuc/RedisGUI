e3229f6da4dda63a47613e0e0ecb14dcf27d8b4f
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-05-01 17:51:20 +0700

    feat: refactor main.cpp to initialize logging and handle JSON input

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,8 +1,30 @@
-#include "math_utils.h"
+#include <fmt/base.h>
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+#include <string>
+
+using json = nlohmann::json;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  std::cout << add(2, 3) << std::endl;
+  auto console_logger = spdlog::stderr_color_mt("core");
+  spdlog::set_default_logger(console_logger);
+  spdlog::info("C++ Core initialized successfully!");
+
+  std::string input_line;
+
+  while (std::getline(std::cin, input_line)) {
+    if (input_line.empty())
+      continue;
+
+    json input_json = json::parse(input_line);
+
+    std::string host = input_json["host"];
+    std::string port = input_json["port"];
+
+    std::cout << std::flush;
+  }
+
   return 0;
 }
